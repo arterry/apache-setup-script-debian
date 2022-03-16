e2d7709bd6f90d20f53e6623a2d11a6bd7225803
arterry <arterry@users.noreply.github.com>
2022-03-16 14:38:32 -0700

    Create apache-adjust-default.sh

---
+++ apache-adjust-default.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get install apache2 ssh
+
+#change port to 8080
+sudo sed -i 's/80/8080/' /etc/apache2/ports.conf
+
+#set server name and server admin
+servadmin="debian\@test\.site"
+servname="ServerName test.site"
+sudo cp /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/test-site.conf
+sudo sed -i 's/webmaster\@localhost/$servadmin/' /etc/apache2/sites-enabled/test-site.conf
+sudo sed -i 's/\#ServerName\swww.example.com/$servname/' /etc/apache2/sites-enabled/test-site.conf
+
+#change localhost to test.site, replace first occurance
+sudo sed -i '0,/localhost/s/localhost/test\.site/' /etc/hosts
+
+#restart apache
+#/etc/init.d/apache2 restart
+#systemctl restart apache2.service
